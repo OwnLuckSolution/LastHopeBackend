d5809b49c347bd75775cf5b37c2ff66fe78b7bbf
Abdul Basit <abdbasit108@gmail.com>
2023-11-23 16:28:02 +0500

    port update

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use('/auth', authRoutes);
 app.use('/admin',adminRoutes);
 app.use('/user',userRoutes);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
